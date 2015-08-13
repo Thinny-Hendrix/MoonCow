d8e5fa0c6c0dce19b0b3257626c9935c8c361cc7
Jarred Laptop <jarred.chiswell@gmail.com>
2015-08-13 15:17:59 +1000

    Some base classes and git test

---
+++ MoonCow/MoonCow/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MoonCow
+{
+    class Camera : Microsoft.Xna.Framework.GameComponent
+    {
+        public Matrix viewMatrix { get; protected set; }
+        public Matrix projectionMatrix { get; protected set; }
+
+        public Camera(Game game, Vector3 pos, Vector3 target, Vector3 up) : base(game)
+        {
+            viewMatrix = Matrix.CreateLookAt(pos, target, up);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4,
+                (float)game.Window.ClientBounds.Width / (float)game.Window.ClientBounds.Height,
+                1, 100);
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ MoonCow/MoonCow/MapNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoonCow
+{
+    class MapNode
+    {
+        // do a shit ton of stuff in here
+    }
+}
