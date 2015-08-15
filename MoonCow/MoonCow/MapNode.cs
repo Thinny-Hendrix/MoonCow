b1e7abdf4e018c48b0cfaae74e38fd04b6ee4073
Thinny-Hendrix <jarred.chiswell@gmail.com>
2015-08-15 11:17:13 +1000

    Begun work on mapnode

--- MoonCow/MoonCow/MapNode.cs
+++ MoonCow/MoonCow/MapNode.cs
@@ -2,19 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace MoonCow
 {
+    /// <summary>
+    /// A node in the map. This will be represented by a tile model in the 3D world.
+    /// This node is used for many things, primarily pathfinding.
+    /// </summary>
     class MapNode
     {
-        //See searchnode in tutorial
-        bool traversable;
+        public bool traversable;       //Can this node be walked through?
+        public Vector2 position;       //X and Y co-ordinates in map
+        public MapNode[] neighbors;    //Array of linked nodes
+        public MapNode parent;         //Pointer to node in path before this one
+
+        public bool inOpenList;        //Currently in OpenList?
+        public bool inClosedList;      //Currently in ClosedList?
+
+        public float distanceToGoal;   //How far to goal
+        public float distanceSoFar;    //How far along in the path this node is
+
+        public float damage;           //How much damage this square can take from turrets
+        public float playerDamage;     //How much damage the player can currently do to this node
         
+
+        /// <summary>
+        /// Constructor for MapNode. Sets a lot of data based on the type of node it is.
+        /// </summary>
         public MapNode()
         {
-            // Gunna be some crazy shit up in here
+            // set data, do lots of other shit too
         }
-
     }
 }
   
---
+++ MoonCow/MoonCow/Pathfinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MoonCow
+{
+    class Pathfinder
+    {
+        // A lot of work needs to be done here.
+    }
+}
