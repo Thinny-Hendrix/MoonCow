3273b487a405bbfa0f6656e6c297e4ec1c9a364d
Frank Elliott <franke215@hotmail.com>
2015-08-22 15:51:53 +1000

    Xml into a 2D array of ints
    Just create a MapData object with an xml doc input -
    i.e. new MapData("/maps/map1.xml")
    Then use the get methods to retrieve data -
    to get the 2D array it is "getNodes()"
    The other main one you'd want is probably "getName()" which will return
    the map name

---
+++ MoonCow/MoonCow/MapData.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace MoonCow
+{
+    class MapData
+    {
+        int id;
+        String name;
+        String creator;
+        int width;
+        int length;
+        int[,] map;
+        String fileName;
+
+        public MapData(String fileName)
+	    {
+            this.fileName = fileName;
+            read(fileName);
+	    }
+
+        private void read(String fileName) //using xpath
+        {
+            List<int> nodes = new List<int>();
+
+            XPathDocument file = new XPathDocument(fileName);
+            XPathNavigator nav = file.CreateNavigator();
+            XPathNodeIterator iterator = nav.Select("//tiles/tile");
+
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    nodes.Add(Int32.Parse(iterator.Current.Value));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Map does not contain any tiles");
+            }
+
+            iterator = nav.Select("//data/width");
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    width = Int32.Parse(iterator.Current.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Width Error");
+            }
+
+            iterator = nav.Select("//data/ID");
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    id = Int32.Parse(iterator.Current.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("ID Error");
+            }
+
+            iterator = nav.Select("//data/name");
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    name = iterator.Current.Value;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name Error");
+            }
+
+            iterator = nav.Select("//data/creator");
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    creator = iterator.Current.Value;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Creator Error");
+            }
+          
+            iterator = nav.Select("//data/length");
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    length = Int32.Parse(iterator.Current.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Length Error");
+            }
+
+            map = convertTo2D(width, length, nodes);
+        }
+
+        private int[,] convertTo2D(int width, int length, List<int> nodes)
+        {
+            int[,] map = new int[length, width];
+            int count = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int q = 0; q < width; q++)
+                {
+                    map[i, q] = nodes[count];
+                    count++;
+                }
+            }
+            return map;
+        }
+
+        public int[,] getNodes()
+        {
+            return map;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getCreator()
+        {
+            return creator;
+        }
+
+        public int getWidth()
+        {
+            return width;
+        }
+
+        public int getLength()
+        {
+            return length;
+        }
+    }
+}
